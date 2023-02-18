a33952b19b01c77c0ede67ca509c561a328f06b4
Ilker Manap <ilkermanap@gmail.com>
2023-02-18 12:55:09 +0100

    Revert "added support for missing price number reset case. Raise alarm for th…"

--- kujira/models.py
+++ kujira/models.py
@@ -93,13 +93,7 @@ class User(Base):
         if len(self.missing) > 0:
             last = self.missing[-1]
             if self.last_alarm_date is not None:
-                #if counter resets, raise alarm
-                if 
-                    
-                if (
-                        (last.value - self.last_alarm_count >= self.alarm_threshold) or
-                        (last.value < self.last_alarm_count)  # if the count resets, raise alarm also.
-                ):
+                if last.value - self.last_alarm_count >= self.alarm_threshold:
                     self.last_alarm_date = last.date
                     self.last_alarm_count = last.value
                     session.commit()
